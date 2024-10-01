aed181eb119c784385b45bf9dae3677f01ab510f
JavGarin <hlm.javiergarin@gmail.com>
2024-10-01 13:55:15 -0300

    changes Hito 3

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Home from './components/Home';
 import Footer from './components/Footer';
 import Register from './components/Register';
 import Login from './components/Login';
+import Cart from "./components/Cart";
 
 const App = () => {
     return (
     <div>
         <Navbar />
         <Home />
-        <Register />
-        <Login />
+        {/* <Register /> */}
+        {/* <Login /> */}
+        <Cart />
         <Footer />
     </div>
     );
