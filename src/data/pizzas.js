ba1a2c6e388d4c2c1fb6845887899d7dbcad0ee3
JavGarin <hlm.javiergarin@gmail.com>
2024-10-01 14:11:39 -0300

    adding pizza toppings

---
+++ src/data/pizzas.js
@@ -0,0 +1,64 @@
+// Array de pizzas inventada
+
+export const pizzas = [
+    {
+    id: 1,
+    name: "Margarita",
+    ingredients: ["Tomate", "Mozzarella", "Albahaca"],
+    price: 8.990,
+    image: "https://example.com/margarita.jpg"
+    },
+    {
+    id: 2,
+    name: "Pepperoni",
+    ingredients: ["Pepperoni", "Mozzarella", "Tomate"],
+    price: 9.990,
+    image: "https://example.com/pepperoni.jpg"
+    },
+    {
+    id: 3,
+    name: "Cuatro Quesos",
+    ingredients: ["Mozzarella", "Parmesano", "Gorgonzola", "Ricotta"],
+    price: 10.990,
+    image: "https://example.com/cuatro-quesos.jpg"
+    },
+    {
+    id: 4,
+    name: "Hawaiana",
+    ingredients: ["Piña", "Jamón", "Mozzarella", "Tomate"],
+    price: 9.490,
+    image: "https://example.com/hawaiana.jpg"
+    },
+    {
+    id: 5,
+    name: "Vegana",
+    ingredients: ["Tomate", "Espinacas", "Aceitunas", "Pimientos"],
+    price: 11.990,
+    image: "https://example.com/vegana.jpg"
+    },
+    {
+    id: 6,
+    name: "Mexicana",
+    ingredients: ["Chorizo", "Jalapeños", "Mozzarella", "Tomate"],
+    price: 10.990,
+    image: "https://example.com/mexicana.jpg"
+    }
+];
+
+  // este array para simular el carrito de compras
+export const pizzaCart = [
+    {
+    id: 1,
+    name: "Margarita",
+    price: 8.990,
+    quantity: 2,
+    image: "https://example.com/margarita.jpg"
+    },
+    {
+    id: 2,
+    name: "Pepperoni",
+    price: 9.990,
+    quantity: 1,
+    image: "https://example.com/pepperoni.jpg"
+    }
+];
