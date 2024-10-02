534cbf72582de31e832ab74ed056f455b24d2058
JavGarin <hlm.javiergarin@gmail.com>
2024-10-02 19:39:19 -0300

    deleting files

--- src/data/pizzas.js
+++ src/data/pizzas.js
@@ -1,23 +1,23 @@
 export const pizzas = [
-  {
-      id: 1,
-      name: "Pizza Margherita",
-      price: 8000,
-      image: "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c", // Imagen de la pizza
-      description: "Tomate, mozzarella, albahaca."
-  },
-  {
-      id: 2,
-      name: "Pizza Pepperoni",
-      price: 9000,
-      image: "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fcheese-164872_640_com.jpg?alt=media&token=18b2b821-4d0d-43f2-a1c6-8c57bc388fab", // Imagen de la pizza
-      description: "Tomate, mozzarella, pepperoni."
-  },
-  {
-      id: 3,
-      name: "Pizza Vegetariana",
-      price: 8500,
-      image: "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-ac54-90f6c31eb3e3", // Imagen de la pizza
-      description: "Tomate, mozzarella, vegetales."
-  }
+{
+    id: 1,
+    name: "Pizza Margherita",
+    price: 8000,
+    image: "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c", // Imagen de la pizza
+    description: "Tomate, mozzarella, albahaca."
+},
+{
+    id: 2,
+    name: "Pizza Pepperoni",
+    price: 9000,
+    image: "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fcheese-164872_640_com.jpg?alt=media&token=18b2b821-4d0d-43f2-a1c6-8c57bc388fab", // Imagen de la pizza
+    description: "Tomate, mozzarella, pepperoni."
+},
+{
+    id: 3,
+    name: "Pizza Vegetariana",
+    price: 8500,
+    image: "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-ac54-90f6c31eb3e3", // Imagen de la pizza
+    description: "Tomate, mozzarella, vegetales."
+}
 ];
