32ac7035aba7416707a6c844d263871cdd93b59b
JavGarin <hlm.javiergarin@gmail.com>
2024-10-26 18:19:03 -0300

    Add or update context files for cart functionality

---
+++ src/context/CartContext.js
@@ -0,0 +1,52 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const CartContext = createContext();
+
+export const useCart = () => useContext(CartContext);
+
+export const CartProvider = ({ children }) => {
+    const [cart, setCart] = useState([]);
+
+    const addToCart = (pizza) => {
+        setCart(prevCart => {
+            const existingPizza = prevCart.find(item => item.id === pizza.id);
+            if (existingPizza) {
+                return prevCart.map(item =>
+                    item.id === pizza.id
+                        ? { ...item, quantity: item.quantity + 1 }
+                        : item
+                );
+            } else {
+                return [...prevCart, { ...pizza, quantity: 1 }];
+            }
+        });
+    };
+
+    const removeFromCart = (id) => {
+        setCart(prevCart => prevCart.filter(item => item.id !== id));
+    };
+
+    const increaseQuantity = (id) => {
+        setCart(prevCart =>
+            prevCart.map(item =>
+                item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+            )
+        );
+    };
+
+    const decreaseQuantity = (id) => {
+        setCart(prevCart =>
+            prevCart.map(item =>
+                item.id === id && item.quantity > 1 ? { ...item, quantity: item.quantity - 1 } : item
+            )
+        );
+    };
+
+    const totalPrice = cart.reduce((total, item) => total + item.price * item.quantity, 0);
+
+    return (
+        <CartContext.Provider value={{ cart, addToCart, removeFromCart, increaseQuantity, decreaseQuantity, totalPrice }}>
+            {children}
+        </CartContext.Provider>
+    );
+};
