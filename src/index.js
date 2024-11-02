9381fa1d08e9508176f161e3168ae3ccbcdf04d0
JavGarin <hlm.javiergarin@gmail.com>
2024-11-02 12:22:09 -0300

    index.js up date

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
+import { UserProvider } from './context/UserContext'; // importo el UserProvider
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <UserProvider>
+                <CartProvider>
+                    <App />
+                </CartProvider>
+            </UserProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
