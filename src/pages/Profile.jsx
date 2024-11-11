7ff93dad4da7d3da3f0b3423b3374063d9f8a496
JavGarin <hlm.javiergarin@gmail.com>
2024-11-10 23:48:39 -0300

    Profile.jsx up date

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useUser } from '../context/UserContext';
+import { useNavigate } from 'react-router-dom';
 
 const Profile = () => {
-    const email = "javiergarin@example.com";
+    const { getProfile, logout } = useUser();
+    const [userEmail, setUserEmail] = useState('');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchProfile = async () => {
+            const profile = await getProfile();
+            if (profile && profile.email) { // verifica si profile existe y si tiene email
+                setUserEmail(profile.email);
+            } else {
+                console.error('Error al cargar el perfil o el perfil no contiene email');
+                navigate('/login'); // redirige al login si el perfil no es válido
+            }
+        };
+        fetchProfile();
+    }, [getProfile, navigate]);
 
     const handleLogout = () => {
-
-        console.log('Logout clicked');
+        logout();
+        navigate('/login'); // redirige al login después del logout
     };
 
     return (
         <div className="container mt-5">
             <h2>Perfil 🍕</h2>
-            <p><strong>Email:</strong> {email}</p>
-            <p><strong>Nivel:</strong> ⭐⭐⭐⭐⭐</p>
-            <p><strong>Puntos acumulados:</strong> 1934/2000⚡</p>
-            <p><strong>Descuento:</strong> 15% 😉</p>
-            <button className="btn btn-danger" onClick={handleLogout}>Cerrar sesión</button>
+            {userEmail ? (
+                <>
+                    <p><strong>Nick:</strong> Javier Garin <strong>G-76</strong></p>
+                    <p><strong>Email:</strong> {userEmail}</p>
+                    <p><strong>Nivel:</strong> ⭐⭐⭐⭐⭐</p>
+                    <p><strong>Puntos acumulados:</strong> 1934/2000⚡</p>
+                    <p><strong>Descuento:</strong> 15% 😉</p>
+                    <button className="btn btn-danger" onClick={handleLogout}>Cerrar sesión</button>
+                </>
+            ) : (
+                <p>Cargando perfil...</p>
+            )}
         </div>
     );
 };
 
 export default Profile;
-
